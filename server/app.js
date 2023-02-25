4441f280556d9a4a5696d8bd39a4d115cf9078ae
Prathibha Ratnayake <prsthibha@gmail.com>
2023-02-25 10:57:23 +0530

    bug fix

--- server/app.js
+++ server/app.js
@@ -1,26 +1,26 @@
-const express = require('express')
-const cors = require('cors')
-const morgan = require('morgan')
-const path = require('path')
-const api = require('./routes/api')
-const {errorHandler,notFound} = require('./middleware/error.middleware')
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const path = require('path');
+const api = require('./routes/api');
+const { errorHandler, notFound } = require('./middleware/error.middleware');
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
-app.use(morgan('dev'))
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
 
-app.use('/api', api)
+app.use('/api', api);
 
-if(process.env.NODE_ENV !== 'development') {
+if (process.env.NODE_ENV !== 'development') {
   app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
-
+  app.use('/api', api);
   app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'client', 'build'));
+    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
   });
 }
 
 app.use(notFound);
-app.use(errorHandler)
-module.exports = app
+app.use(errorHandler);
+module.exports = app;
