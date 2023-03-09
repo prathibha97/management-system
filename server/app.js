30462a048cc2986ceb6c6df46ad6beb0d5d37456
Prathibha Ratnayake <prsthibha@gmail.com>
2023-03-09 20:27:43 +0530

    add cache middleware

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const path = require('path');
 const api = require('./routes/api');
 const { errorHandler, notFound } = require('./middleware/error.middleware');
+// const setCache = require('./middleware/cache.middleware');
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(morgan('dev'));
+// app.use(setCache);
 
 app.use('/api', api);
 
