64d2772d99f45b7a2f3f2fb5fafe5d5498615cfb
Prathibha Ratnayake <prsthibha@gmail.com>
2023-03-10 11:07:05 +0530

    Add besic calendar

--- client/src/pages/Dashboard/index.jsx
+++ client/src/pages/Dashboard/index.jsx
@@ -1,20 +1,37 @@
+// import React from 'react'
+
+// function Dashboard() {
+
+//   return (
+//     <div className="grid grid-cols-3 gap-4 mt-5 ">
+//       <div>
+//         <div className='grid gap-4 h-[660px]'>
+//           <div className='bg-[#EEF2F5] rounded-lg'>Ongoing projects</div>
+//           <div className='bg-[#EEF2F5] rounded-lg'>Client detials</div>
+//         </div>
+//       </div>
+//       <div className='grid gap-4'>
+//         <div className='bg-[#EEF2F5] h-[250px] rounded-lg'>
+//           {/* <Calendar /> */}
+//           Calendar
+//         </div>
+//         <div className='bg-[#EEF2F5] h-[390px] rounded-lg'>upcoming events</div>
+//       </div>
+//       <div className='bg-[#EEF2F5] rounded-lg'>todo list</div>
+//     </div>
+//   )
+// }
+
+// export default Dashboard
+
 import React from 'react'
+import Calendar from '../../components/Calendar'
 
 function Dashboard() {
 
   return (
-    <div className="grid grid-cols-3 gap-4 mt-5 ">
-      <div>
-        <div className='grid gap-4 h-[660px]'>
-          <div className='bg-[#EEF2F5] rounded-lg'>Ongoing projects</div>
-          <div className='bg-[#EEF2F5] rounded-lg'>Client detials</div>
-        </div>
-      </div>
-      <div className='grid gap-4'>
-        <div className='bg-[#EEF2F5] h-[250px] rounded-lg'>calander</div>
-        <div className='bg-[#EEF2F5] h-[390px] rounded-lg'>upcoming events</div>
-      </div>
-      <div className='bg-[#EEF2F5] rounded-lg'>todo list</div>
+    <div>
+      <Calendar />
     </div>
   )
 }
