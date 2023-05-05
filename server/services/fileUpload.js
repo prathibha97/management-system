320db98d169209e4abbea4c0f188d7365a33c6b2
Prathibha Ratnayake <prsthibha@gmail.com>
2023-05-05 10:02:23 +0530

    code cleanup

--- server/services/fileUpload.js
+++ server/services/fileUpload.js
@@ -1,38 +1,3 @@
-// /* eslint-disable import/no-extraneous-dependencies */
-// const multer = require('multer');
-
-// const storage = multer.diskStorage({
-//   destination(req, file, cb) {
-//     if (file.fieldname === 'idCardPath') {
-//       cb(null, 'uploads/idCards');
-//     } else if (file.fieldname === 'bankPassPath') {
-//       cb(null, 'uploads/bankPasses');
-//     } else if (file.fieldname === 'resumePath') {
-//       cb(null, 'uploads/resumes');
-//     } else if (file.fieldname === 'projectScope') {
-//       cb(null, 'uploads/projectScopes');
-//     }
-//      else {
-//       cb(null, 'uploads');
-//     }
-//   },
-//   filename(req, file, cb) {
-//     const { firstName, lastName, title } = req.body;
-//     const sanitizedFirstName = firstName?.replace(/\s+/g, '_');
-//     const sanitizedLastName = lastName?.replace(/\s+/g, '_');
-//     const sanitizedTitle = title?.replace(/\s+/g, '_');
-//     cb(null, `${sanitizedFirstName}_${sanitizedLastName}_${file.originalname}` || `${sanitizedTitle}_${file.originalname}`);
-//   },
-// });
-
-// const upload = multer({
-//   storage,
-//   // limits: { fileSize: 50 * 1024 * 1024 }, // 50 megabytes
-// });
-
-// module.exports = upload;
-
-/* eslint-disable import/no-extraneous-dependencies */
 const multer = require('multer');
 
 const storage = multer.diskStorage({
