3b826bfc0df5a2a545d7635877643df2f27977f2
Prathibha Ratnayake <prsthibha@gmail.com>
2023-05-16 11:08:25 +0530

    improve UI

--- client/src/components/TimeSheetMenu/index.jsx
+++ client/src/components/TimeSheetMenu/index.jsx
@@ -1,41 +1,75 @@
 import { Delete, Edit, Visibility } from '@mui/icons-material';
-import { Menu, MenuItem } from '@mui/material';
+import { Menu, MenuItem, Typography, Dialog, DialogTitle, DialogContent, DialogActions, Button } from '@mui/material';
+import React, { useState } from 'react';
 
-import React from 'react';
+function TimeSheetMenu({ anchorEl, handleMenuClose, handleView, handleEdit, handleDelete }) {
+  const [deleteConfirmationOpen, setDeleteConfirmationOpen] = useState(false);
 
+  const handleDeleteConfirmationOpen = () => {
+    setDeleteConfirmationOpen(true);
+    handleMenuClose();
+  };
 
-function TimeSheetMenu({ anchorEl, handleMenuClose, handleView, handleEdit, handleDelete}) {
+  const handleDeleteConfirmationClose = () => {
+    setDeleteConfirmationOpen(false);
+  };
+
+  const handleDeleteConfirmation = () => {
+    setDeleteConfirmationOpen(false);
+    handleDelete();
+  };
+
   return (
-    <Menu
-      anchorEl={anchorEl}
-      open={Boolean(anchorEl)}
-      onClose={handleMenuClose}
-      anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-    >
-      <MenuItem onClick={handleView}>
-        <Visibility fontSize='small' />
-        {' '}
-        View
-      </MenuItem>
-      <MenuItem onClick={handleEdit}>
-        <Edit fontSize='small' />
-        {' '}
-        Edit
-      </MenuItem>
-      <MenuItem onClick={handleDelete}>
-        <Delete fontSize='small' />
-        {' '}
-        Delete
-      </MenuItem>
-    </Menu>
+    <>
+      <Menu
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleMenuClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'start',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'start',
+        }}
+      >
+        <MenuItem onClick={handleView} sx={{ '&:hover': { bgcolor: 'grey.200' } }}>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <Visibility fontSize='small' />
+            <Typography variant='body1' sx={{ ml: 1 }}>View</Typography>
+          </div>
+        </MenuItem>
+        <MenuItem onClick={handleEdit} sx={{ '&:hover': { bgcolor: 'grey.200' } }}>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <Edit fontSize='small' />
+            <Typography variant='body1' sx={{ ml: 1 }}>Edit</Typography>
+          </div>
+        </MenuItem>
+        <MenuItem onClick={handleDeleteConfirmationOpen} sx={{ '&:hover': { bgcolor: 'grey.200' } }}>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <Delete fontSize='small' />
+            <Typography variant='body1' sx={{ ml: 1 }}>Delete</Typography>
+          </div>
+        </MenuItem>
+      </Menu>
+
+      <Dialog open={deleteConfirmationOpen} onClose={handleDeleteConfirmationClose}>
+        <DialogTitle>Delete Confirmation</DialogTitle>
+        <DialogContent>
+          <Typography variant="body1">Are you sure you want to delete this time log?</Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDeleteConfirmationClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleDeleteConfirmation} color="error" >
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   )
 }
 
-export default TimeSheetMenu
+export default TimeSheetMenu;
