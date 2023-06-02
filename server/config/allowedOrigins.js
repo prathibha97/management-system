04bd4d6da4ecf1acce1c6e0f9befcdf626a272eb
Prathibha Ratnayake <prsthibha@gmail.com>
2023-06-02 11:53:42 +0530

    commit

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -4,6 +4,9 @@ const allowedOrigins = [
   'http://localhost:5000',
   'http://localhost:3500',
   'http://localhost:3000',
+  'http://34.210.201.75:5000',
+  'http://ec2-34-210-201-75.us-west-2.compute.amazonaws.com:5000',
+  'http://ec2-52-88-221-122.us-west-2.compute.amazonaws.com:5001'
 ];
 
 module.exports = allowedOrigins;
