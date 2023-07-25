9f6247f5fbce9f080ef209f3a98e60cbd685ad13
Prathibha Ratnayake <prsthibha@gmail.com>
2023-07-25 15:44:22 +0530

    bug fix

--- server/services/socket.js
+++ server/services/socket.js
@@ -1,6 +1,6 @@
 const io = require('socket.io')(5001, {
   cors: {
-    origin: 'http://34.210.201.75:5000',
+    origin: 'http://localhost:5000',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   },
